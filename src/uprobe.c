17d32738ce97bc00075c82e9a4e312fe52380f79
lipeng hao <lipenghao@qingteng.cn>
2025-06-30 23:45:35 +0800

    learn uprobe tech deep

--- src/uprobe.c
+++ src/uprobe.c
@@ -14,6 +14,7 @@ int main(int argc, char **argv)
 {
 	struct uprobe_bpf *skel;
 	int err;
+	const char *target_program_path;
 	LIBBPF_OPTS(bpf_uprobe_opts, uprobe_opts);
 
 	if (argc != 2) {
@@ -21,6 +22,9 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Example: %s ./target\n", argv[0]);
 		return 1;
 	}
+
+	/* Extract target program path from command line argument */
+	target_program_path = argv[1];
 
 	/* Set up libbpf errors and debug info callback */
 	libbpf_set_print(libbpf_print_fn);
@@ -32,12 +36,16 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	/* Attach uprobe for uprobe_add function */
-	uprobe_opts.func_name = "uprobe_add";
-	uprobe_opts.retprobe = false;
-	skel->links.uprobe_add = bpf_program__attach_uprobe_opts(skel->progs.uprobe_add,
-								 -1 /* all processes */, argv[1],
-								 0 /* offset for function */,
+	/*uprobe/uretprobe 需要指定要附加到的函数的相对偏移量。
+	  如果我们提供函数名，libbpf 将自动为我们查找偏移量。
+	  如果没有指定函数名，libbpf 将尝试使用函数偏移量。
+	*/
+	uprobe_opts.func_name = "uprobe_add";//要附加的函数名称
+	uprobe_opts.retprobe = false; //是否为返回类型探针
+	skel->links.uprobe_add = bpf_program__attach_uprobe_opts(
+								skel->progs.uprobe_add/* 要附加的bpf程序 */,
+								 -1 /* all processes */, target_program_path,
+								 0 /* 函数偏移量,因使用opts.func_name，所以设置为0 */,
 								 &uprobe_opts /* opts */);
 	if (!skel->links.uprobe_add) {
 		err = -errno;
@@ -49,18 +57,20 @@ int main(int argc, char **argv)
 	uprobe_opts.func_name = "uprobe_add";
 	uprobe_opts.retprobe = true;
 	skel->links.uretprobe_add = bpf_program__attach_uprobe_opts(
-		skel->progs.uretprobe_add, -1 /* all processes */, argv[1],
-		0 /* offset for function */, &uprobe_opts /* opts */);
+		skel->progs.uretprobe_add,
+		-1 /* all processes */, target_program_path,
+		0 /* offset for function */, 
+		&uprobe_opts /* opts */);
 	if (!skel->links.uretprobe_add) {
 		err = -errno;
 		fprintf(stderr, "Failed to attach uretprobe for uprobe_add: %d\n", err);
 		goto cleanup;
 	}
 
-	printf("Successfully attached uprobes to %s\n", argv[1]);
+	printf("Successfully attached uprobes to %s\n", target_program_path);
 	printf("Monitoring functions: uprobe_add\n");
 	printf("Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` in another terminal to see output.\n");
-	printf("Then run the target program: %s\n", argv[1]);
+	printf("Then run the target program: %s\n", target_program_path);
 	printf("Press Ctrl+C to exit.\n");
 
 	/* Keep the program running to monitor function calls */
